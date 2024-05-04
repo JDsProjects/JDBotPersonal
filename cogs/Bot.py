bc4ad5f4000ebcecaee84f19c61488132b78111f
JDJG <ghostzap123@gmail.com>
2024-05-03 22:10:00 -0400

    Got rid of unneeded f string.

--- cogs/Bot.py
+++ cogs/Bot.py
@@ -18,7 +18,7 @@ class Bot(commands.Cog):
     async def status_task(self):
         await self.bot.change_presence(
             status=discord.Status.online,
-            activity=discord.Activity(type=discord.ActivityType.listening, name=f"Personal Bot of JDJG"),
+            activity=discord.Activity(type=discord.ActivityType.listening, name="Personal Bot of JDJG"),
         )
         await asyncio.sleep(40)
         await self.bot.change_presence(
